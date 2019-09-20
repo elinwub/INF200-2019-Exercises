829acba9011f7a17e0b868ebbaa6680c28ae124d
Elin Bjørnson <elinbj@nmbu.no>
2019-09-20 11:21:28 +0200

    Started exercise 2

---
+++ src/elin_bjoernson_ex/ex02/message_entropy.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+def letter_freq(txt):
+    txt = txt.lower()  # make all letters lowercase
+    txt = sorted(txt)  # sort alphabetically
+    characters = {}    # make a dictionary to sort letters
+    for character in txt:
+        if character in characters.keys():
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
+
+
+def entropy(message):
+    pass
+
+
+if __name__ == "__main__":
+    for msg in '', 'aaaa', 'aaba', 'abcd', 'This is a short text.':
+        print('{:25}: {:8.3f} bits'.format(msg, entropy(msg)))
