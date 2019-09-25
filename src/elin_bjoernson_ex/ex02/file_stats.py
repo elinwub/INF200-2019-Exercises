8a2dcee1c4b9fb966c569c080f20bf71e4e47329
Elin Bjørnson <elinbj@nmbu.no>
2019-09-25 16:37:37 +0200

    finished file_stats

--- src/elin_bjoernson_ex/ex02/file_stats.py
+++ src/elin_bjoernson_ex/ex02/file_stats.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
+
+
 def char_counts(textfilename):
-    pass
+    infile = open(textfilename, encoding='utf-8')
+    file_string = infile.read().strip()  # reads file and removes whitespace
+    character_count = {i: 0 for i in list(range(256))}  # make dictionary of utf-codes with count 0
+    for c in file_string:
+        character_count[ord(c)] += 1
+    result = list(character_count.values())
+    infile.close()
+    return result
 
 
 if __name__ == '__main__':
