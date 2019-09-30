559c88334ccc83f14a93d800a17ff69b10e94546
Elin Bjørnson <elinbj@nmbu.no>
2019-09-30 12:49:01 +0200

    Made tests

--- src/elin_bjoernson_ex/ex03_project/test_sorting.py
+++ src/elin_bjoernson_ex/ex03_project/test_sorting.py
@@ -8,31 +8,21 @@ def bubble_sort(numbers):
     new_list = list(numbers)
     for _ in new_list:
         for idx, num in enumerate(new_list):
-            if idx == len(new_list)-1:  # if last element, do nothing
+            if idx == len(new_list) - 1:  # if last element, do nothing
                 pass
-            elif num > new_list[idx+1]:  # if higher value than next element, exchange them
-                new_list.insert(idx+1, new_list.pop(idx))
+            elif num > new_list[idx + 1]:
+                new_list.insert(idx + 1, new_list.pop(idx))
     return new_list
 
 
-if __name__ == "__main__":
-
-    for data in ((),
-                 (1,),
-                 (1, 3, 8, 12),
-                 (12, 8, 3, 1),
-                 (8, 3, 12, 1),):
-        print('{!s:>15} --> {!s:>15}'.format(data, bubble_sort(data)))
-
-
-    def test_empty():
-        """Test that the sorting function works for empty list"""
-pass
+def test_empty():
+    """Test that the sorting function works for empty list"""
+    assert bubble_sort([]) == []
 
 
 def test_single():
     """Test that the sorting function works for single-element list"""
-    pass
+    assert bubble_sort([1]) == [1]
 
 
 def test_sorted_is_not_original():
@@ -47,7 +37,9 @@ def test_sorted_is_not_original():
     Now sorted_data shall be a different object than data,
     not just another name for the same object.
     """
-    pass
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+    assert sorted_data != data
 
 
 def test_original_unchanged():
@@ -61,22 +53,30 @@ def test_original_unchanged():
 
     Now data shall still contain [3, 2, 1].
     """
-    pass
+    data = [3, 2, 1]
+    bubble_sort(data)
+    assert data == [3, 2, 1]
 
 
 def test_sort_sorted():
     """Test that sorting works on sorted data."""
-    pass
+    data = [1, 2, 3]
+    sorted_data = bubble_sort(data)
+    sort_sorted = bubble_sort(sorted_data)
+    assert sort_sorted == sorted_data
 
 
 def test_sort_reversed():
     """Test that sorting works on reverse-sorted data."""
-    pass
+    data = [1, 3, 2]
+    reverse_sorted = bubble_sort(data)[::-1]
+    assert bubble_sort(reverse_sorted) == sorted(data)
 
 
 def test_sort_all_equal():
     """Test that sorting handles data with identical elements."""
-    pass
+    data = [1, 2, 1]
+    assert bubble_sort(data) == sorted(data)
 
 
 def test_sorting():
@@ -87,4 +87,11 @@ def test_sorting():
     ensure that they are sorted correctly. These could be lists of
     numbers of different length or lists of strings.
     """
-    pass
+    data = ([1],
+            [1, 20, 3, 5],
+            [9, 8, 7],
+            [12, 20, 11, 109],
+            [9, 2, 88, 3, 10, 22])
+    for numbers in data:
+        sorted_data = bubble_sort(numbers)
+        assert sorted_data == sorted(numbers)
