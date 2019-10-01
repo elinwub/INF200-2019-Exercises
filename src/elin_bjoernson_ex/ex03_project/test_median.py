e25ea7a8d0e2eb11d5ff87d13ea43911d5a81c39
Elin Bjørnson <elinbj@nmbu.no>
2019-10-01 13:25:27 +0200

    made functions for tests

--- src/elin_bjoernson_ex/ex03_project/test_median.py
+++ src/elin_bjoernson_ex/ex03_project/test_median.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-
-__author__ = 'Elin Bjoernson'
-__email__ = 'elinbj@nmbu.no'
+import pytest
+#from .median import median
+__author__ = "Elin Bjoernson"
+__email__ = "elinbj@nmbu.no"
 
 # function from exercise text
 def median(data):
@@ -12,7 +13,64 @@ def median(data):
     :return: Median of data
     """
 
-    sdata = sorted(data)
-    n = len(sdata)
-    return (sdata[n//2] if n % 2 == 1
-        else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
+    sorted_data = sorted(data)
+    num_elements = len(sorted_data)
+    if num_elements % 2 == 1:
+        return sorted_data[num_elements // 2]
+    else:
+        return (
+            sorted_data[num_elements // 2 - 1] + sorted_data[num_elements // 2]
+        ) / 2
+
+
+def test_median_of_singleton():
+    assert median([4]) == 4
+
+
+def test_median_raises_value_error_on_empty_list():
+    """Test checking that requesting the median of an empty list raises a
+    ValueError exception"""
+    with pytest.raises(ValueError):
+        median([])
+    # supposed to fail?
+    #samme som:
+    #try:
+    #    median([])
+    #except ValueError:
+    #    pass
+    #else:
+    #    assert False
+
+
+def test_odd_numbers():
+    """Tests that median works on list with odd number of elements
+    Returns
+    -------
+
+    """
+
+
+def test_even_numbers():
+    """Tests that median works on list with even numbers of elements
+    """
+
+
+def test_ordered_numbers():
+    """Tests that median works on ordered elements
+    """
+
+
+def test_reverse_ordered_numbers():
+    """Tests that median works on reverse-ordered elements"""
+
+
+def test_unordered_numbers():
+    """Tests that median works on unordered elements"""
+
+
+def test_original_unchanged():
+    """Tests that median function leaves the original data unchanged"""
+
+
+def test_tuples():
+    """Tests that median function works for tuples"""
