8b16ad84d12dfd4eb39ae82e75847c2e1381c92e
Elin Bjørnson <elinbj@nmbu.no>
2019-10-23 15:30:13 +0200

    Added classes

---
+++ src/elin_bjoernson_ex/ex04/myrand.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Elin Bjørnson'
+__email__ = 'elinbj@nmbu.no'
+
+
+class LCGRand:
+    def __init__(self):
+        pass
+
+    def rand(self):
+        """ Returns the next random number"""
+        pass
+
+
+class ListRand:
+    def __init__(self):
+        pass
+
+    def rand(self):
+        """ Returns the next random number"""
+        pass
+
+
---
+++ src/elin_bjoernson_ex/ex04/walker.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Elin Bjørnson'
+__email__ = 'elinbj@nmbu.no'
+
+
+class Walker:
+    def __init__(self):
+
+    def move(self):
+        pass
+
+    def is_at_home(self):
+        pass
+
+    def get_position(self):
+        pass
+
+    def get_steps(self):
+        pass
+
