68ff6cad6d8aa8d9437809cc45efbf78378299c2
Elin Bjørnson <elinbj@nmbu.no>
2019-10-25 15:15:22 +0200

    Made changes on walker

--- src/elin_bjoernson_ex/ex04/walker.py
+++ src/elin_bjoernson_ex/ex04/walker.py
@@ -1,21 +1,51 @@
 # -*- coding: utf-8 -*-
+import random
 
 __author__ = 'Elin Bjørnson'
 __email__ = 'elinbj@nmbu.no'
 
 
 class Walker:
-    def __init__(self):
+    def __init__(self, position, home):
+        self.position = position
+        self.home = home
+        self.moves = 0
 
     def move(self):
-        pass
+        left_or_right = [-1, 1]
+        self.position += random.choice(left_or_right)
+        self.moves += 1
 
     def is_at_home(self):
-        pass
+        if self.position == self.home:
+            return True
+        else:
+            return False
 
     def get_position(self):
-        pass
+        return self.position
 
     def get_steps(self):
-        pass
+        return self.moves
+
+
+def walk(dist, pos, h):
+    for i in range(len(dist)):
+        moves = []
+        for j in range(5):
+            walker = Walker(pos[i], h[i])
+            while walker.is_at_home() == False:
+                walker.move()
+
+            moves.append(walker.get_steps())
+        return print(f'Distance: {dist[i]} -> Path lengths: {moves}')
+
+
+if __name__ == '__main__':
+    distances = [1, 2, 5, 10, 20, 50]
+    initial_positions = [random.randint(0, 100) for i in range(len(distances))]
+    home = [(x+y) for x, y in zip(distances, initial_positions)]
+    print(walk(distances, initial_positions, home))
+
+
 
