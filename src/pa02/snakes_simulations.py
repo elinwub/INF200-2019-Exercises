eb6e997763a525bc628eda4ef5157b0f4d4d5791
Elin Bjørnson <elinbj@nmbu.no>
2019-11-12 14:47:10 +0100

    added board dict

--- src/pa02/snakes_simulations.py
+++ src/pa02/snakes_simulations.py
@@ -5,7 +5,8 @@ __email__ = 'jaer@nmbu.no', 'elinbj@nmbu.no'
 
 
 class Board:
-    std_board = {}
+    std_board = {1: 40, 8: 10, 36: 52, 43: 62, 49: 79, 65: 82, 68: 85,
+                 24: 5, 33: 3, 42: 30, 56: 37, 64: 27, 74: 12, 87: 70}
 
     def __init__(self, ladders, chutes, goal):
         self.ladders = None
